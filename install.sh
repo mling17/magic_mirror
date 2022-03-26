9b410d5b0714decaf455e7cf759fb35ec93a39fc
mling17 <mling17@163.com>
2022-03-26 23:12:15 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
+# update repo
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y redis 
-mkdir /home/pi/.pip/
+# install app
+sudo apt install -y redis
+# 创建pip配置文件
+if [ ! -d "/home/pi/.pip/" ];then
+  mkdir /home/pi/.pip/
+else
+  echo "文件夹已经存在"
+fi
 echo -e "[global]
 timeout =6000
 index-url =http://pypi.douban.com/simple/
@@ -10,3 +17,8 @@ index-url =http://pypi.douban.com/simple/
 use-mirrors =true 
 mirrors =http://pypi.douban.com/simple/ 
 trusted-host =pypi.douban.com" > /home/pi/.pip/pip.conf
+# export
+export PI_LOCATION='潍坊'
+# pip install
+
+# migrate
