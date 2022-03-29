bf5fe79ab65d95f690399ecb33d88430a56b3d33
ming.chen <ming.chen26@pactera.com>
2022-03-29 14:11:43 +0800

    new file:   processor/lunar_festival.py 	modified:   tests/test2.py 	renamed:    tests/test.py -> tests/test_dht11.py

--- tests/test2.py
+++ tests/test2.py
@@ -1,26 +1,16 @@
-from datetime import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
 import time
-import schedule
-import threading
-from lunar_festival import DateInfo
-import get_weather
-import interactive
+import datetime
 
-# schedule.every().hours.do(do_task_thread, date_info.main)  # 每小时更新一次农历日期和节日
-# schedule.every(30).minutes.do(do_task_thread, get_weather.main)  # 每30分钟更新一次天气
-# schedule.every(30).minutes.do(do_task_thread, interactive.main)  # 每30分钟更新一次交互信息
 
-sched = BlockingScheduler()
+def seconds_to_tomorrow():
+    """
+    当前时间到第二天剩余的秒数
+    """
+    today = datetime.date.today()
+    tomorrow = today.replace(day=today.day + 1)
+    tomorrow = datetime.datetime(year=tomorrow.year, month=tomorrow.month, day=tomorrow.day)
+    ex = round(tomorrow.timestamp() - time.time())
+    return ex
 
-today = datetime.today()
 
-date_info = DateInfo()
-start_date = '%s-%s-%s %s:%s:%s' % (today.year, today.month, today.day, today.hour, today.minute, today.second)
-sched.add_job(date_info.run, 'interval', hours=1, start_date=start_date)  # 每小时更新一次农历日期和节日
-sched.add_job(get_weather.run, 'interval', minutes=30)  # 每30分钟更新一次天气
-sched.add_job(interactive.run, 'interval', minutes=30)  # 每30分钟更新一次交互信息
-# 清理redis数据
-start_date = '%s-%s-%s 0:0:2' % (today.year, today.month, today.day)
-sched.add_job(date_info.run, 'interval', hours=24, start_date=start_date)
-sched.start()
+print(seconds_to_tomorrow())
